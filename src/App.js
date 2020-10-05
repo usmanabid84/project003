e51f9b38af4c292af8678a8eaaee15bfe20399e5
Usman Abid <usmanabid84@gmail.com>
2020-10-05 14:39:41 +0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <br/>
+        Made by Usman Abid
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
